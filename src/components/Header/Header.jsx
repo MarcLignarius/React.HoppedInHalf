c7aff21549db39c39968ec42da054326156421b8
Student <example@example.com>
2019-07-16 16:49:49 -0700

    add buttons and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header(){
   return (
-    <h1 className={'title'}>Hopped In Half Taproom</h1>
+    <div>
+      <h1 className={'title'}>Hopped In Half Taproom</h1>
+      <hr/>
+      <Link to="/"><button className={'w3-button w3-white w3-border'}>Home</button></Link>
+      <div className={'divider'}></div>
+      <Link to="/addbeer"><button className={'w3-button w3-white w3-border'}>Add Beer</button></Link>
+      <hr/>
+    </div>
   );
 }
 
