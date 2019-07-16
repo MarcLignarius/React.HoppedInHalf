d52ce204ca1a5904d9a269a9c32b0a396241be45
Student <example@example.com>
2019-07-16 15:36:23 -0700

    add styles to index

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { AppContainer } from 'react-hot-loader';
+import './index.css';
 
 const render = (Component) => {
   ReactDOM.render(
