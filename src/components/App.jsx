c501eb42dcb9f64610900a8f4ae5640b722d0e01
Student <example@example.com>
2019-07-16 11:31:33 -0700

    create Header component and import in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import Header from "./Header";
 
-function App(){
-  return (
-    <div>
-    <h1>Beer Menu</h1>
-    <p>Name: Tricerahops</p>
-    <p>Brewery: Ninkasi Brewing Company, Eugene, OR</p>
-    <p>Style: IPA - Double West Coast</p>
-    <p>ABV: 8% / IBU: 84</p>
-    <p>Price: $7</p>
-    <p>Quantity: 124</p>
-    <hr/>
-  </div>
-  );
+function App() {
+	return (
+		<div>
+			<Header />
+			<h1>Beer Menu</h1>
+			<p>Name: Tricerahops</p>
+			<p>Brewery: Ninkasi Brewing Company, Eugene, OR</p>
+			<p>Style: IPA - Double West Coast</p>
+			<p>ABV: 8% / IBU: 84</p>
+			<p>Price: $7</p>
+			<p>Quantity: 124</p>
+			<hr />
+		</div>
+	);
 }
 
 export default App;
