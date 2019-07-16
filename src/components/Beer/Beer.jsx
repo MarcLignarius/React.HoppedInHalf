365dee24f3d4759a584978dc4f7c9339e3f6d658
Student <example@example.com>
2019-07-16 12:05:13 -0700

    Add props and PropTypes

--- src/components/Beer/Beer.jsx
+++ src/components/Beer/Beer.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Beer() {
+function Beer(props) {
 	return (
 		<div>
 			<h1>Beer Menu</h1>
-			<p>Name: Tricerahops</p>
-			<p>Brewery: Ninkasi Brewing Company, Eugene, OR</p>
-			<p>Style: IPA - Double West Coast</p>
-			<p>ABV: 8% / IBU: 84</p>
-			<p>Price: $7</p>
-			<p>Quantity: 124</p>
+			<p>Name: {props.name}</p>
+			<p>Brewery: {props.brewery}</p>
+			<p>Style: {props.style}</p>
+			<p>ABV: {props.abv}% / IBU: {props.ibu}</p>
+			<p>Price: ${props.price}</p>
+			<p>Pints Left: {props.pintsLeft}</p>
 			<hr/>
 		</div>
   );
 }
+
+Beer.propTypes = {
+  name: PropTypes.string.isRequired,
+  brewery: PropTypes.string.isRequired,
+	style: PropTypes.string.isRequired,
+	abv: PropTypes.number.isRequired,
+	ibu: PropTypes.number.isRequired,
+	price: PropTypes.number.isRequired,
+	pintsLeft: PropTypes.number.isRequired
+};
 
 export default Beer;
--- src/components/BeerList/BeerList.jsx
+++ src/components/BeerList/BeerList.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import Beer from "../Beer/Beer";
+import React from 'react';
+import Beer from '../Beer/Beer';
 
 function BeerList(){
   return (
-    <Beer/>
+    <Beer
+      name='Tricerahops'
+      brewery='Ninkasi Brewing Company, Eugene, OR'
+      style='IPA - Double West Coast'
+      abv={8}
+      ibu={84}
+      price={7}
+      pintsLeft={124}/>
   );
 }
 
