614ee612636c8d52b16919bddd62f8f2ea520a9c
Student <example@example.com>
2019-07-16 11:24:52 -0700

    add index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Beer Menu</h1>
+    <p>Name: Tricerahops</p>
+    <p>Brewery: Ninkasi Brewing Company, Eugene, OR</p>
+    <p>Style: IPA - Double West Coast</p>
+    <p>ABV: 8% / IBU: 84</p>
+    <p>Price: $7</p>
+    <p>Quantity: 124</p>
+  </div>,
+  document.getElementById('react-app-root')
+);
