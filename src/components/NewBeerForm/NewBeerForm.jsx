44018f4d4fc61984ea0199ea135d65d302211c1b
Marc Davies <marcdaviesriot@gmail.com>
2019-07-17 01:09:46 -0700

    add placeholder text for logged in state

--- src/components/NewBeerForm/NewBeerForm.jsx
+++ src/components/NewBeerForm/NewBeerForm.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './NewBeerForm.css';
+import { Link } from 'react-router-dom';
 
 function NewBeerForm(){
   return (
     <div>
+      <h3>You are logged in.</h3>
       <form>
         <input
           type='text'
@@ -36,6 +37,8 @@ function NewBeerForm(){
           placeholder='Pints Left'/>
         <div className={'smallSpace'}></div>
         <button className={'w3-button w3-white w3-border'} type='submit'>Add Beer</button>
+        <div className={'divider'}></div>
+        <Link to="/"><button className={'w3-button w3-white w3-border'}>Logout</button></Link>
       </form>
     </div>
   );
