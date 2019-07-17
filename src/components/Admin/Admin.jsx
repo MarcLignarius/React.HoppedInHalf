f1782829037a0a27b021c48655bf916caa9a41f3
Marc Davies <marcdaviesriot@gmail.com>
2019-07-16 23:31:49 -0700

    create Admin component

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Admin extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <p>This is the Admin component!</p>
+      </div>
+    );
+  }
+}
+
+export default Admin;
