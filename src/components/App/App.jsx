6fb1f4aea5cdbe7e6620be84f0cd1f394dc1dd3f
Marc Davies <marcdaviesriot@gmail.com>
2019-07-16 23:31:32 -0700

    add route to Admin page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from "../Header/Header";
-import BeerList from "../BeerList/BeerList";
-import NewBeerForm from "../NewBeerForm/NewBeerForm";
+import Header from '../Header/Header';
+import Error404 from '../Error404/Error404';
+import Admin from '../Admin/Admin';
+import BeerList from '../BeerList/BeerList';
+import NewBeerForm from '../NewBeerForm/NewBeerForm';
 
 function App() {
-	return (
-		<div>
-			<Header/>
-			<Switch>
-				<Route exact path='/' component={BeerList} />
-				<Route exact path='/addbeer' component={NewBeerForm} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={BeerList} />
+        <Route exact path='/admin' component={Admin} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
