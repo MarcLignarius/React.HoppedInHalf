9c7173d7290b0b6f90fa0e0602307ed409451e90
Marc Davies <marcdaviesriot@gmail.com>
2019-07-17 01:51:40 -0700

    lift masterBeerList from BeerList to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,95 @@ import Header from '../Header/Header';
 import Error404 from '../Error404/Error404';
 import AdminControl from '../AdminControl/AdminControl';
 import BeerList from '../BeerList/BeerList';
+import oldrasputin from '../../assets/labels/oldrasputin.jpg';
+import papyrusiris from '../../assets/labels/papyrusiris.jpeg';
+import guavasocksandsandals from '../../assets/labels/guavasocksandsandals.png';
+import tricerahops from '../../assets/labels/tricerahops.jpg';
+import saisondelis from '../../assets/labels/saisondelis.jpeg';
+import denimvestpionage from '../../assets/labels/denimvestpionage.jpg';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={BeerList} />
-        <Route exact path='/addbeer' component={AdminControl} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterBeerList: [
+        {
+          label: oldrasputin,
+          name: 'Old Rasputin (Nitro)',
+          brewery: 'North Coast Brewing Company, Fort Bragg, CA',
+          style: 'Stout - Russian Imperial',
+          abv: 9,
+          ibu: 75,
+          price: 8,
+          pintsLeft: 124
+        },
+        {
+          label: papyrusiris,
+          name: 'Papyrus Iris',
+          brewery: 'Ruse Brewing, Portland, OR',
+          style: 'IPA - New England',
+          abv: 6.5,
+          ibu: 37,
+          price: 7,
+          pintsLeft: 124
+        },
+        {
+          label: guavasocksandsandals,
+          name: 'Guava Socks & Sandals',
+          brewery: 'Stickmen Brewing Company, Lake Oswego, OR',
+          style: 'IPA - New England',
+          abv: 6.6,
+          ibu: 37,
+          price: 7,
+          pintsLeft: 124
+        },
+        {
+          label: tricerahops,
+          name: 'Tricerahops',
+          brewery: 'Ninkasi Brewing Company, Eugene, OR',
+          style: 'IPA - Double West Coast',
+          abv: 8,
+          ibu: 84,
+          price: 7,
+          pintsLeft: 124
+        },
+        {
+          label: saisondelis,
+          name: 'Saison de Lis',
+          brewery: 'Perennial, St Louis, MO',
+          style: 'Farmhouse Ale - Saison',
+          abv: 5,
+          ibu: 20,
+          price: 8,
+          pintsLeft: 124
+        },
+        {
+          label: denimvestpionage,
+          name: 'Denim Vest-pionage',
+          brewery: 'Baerlic, Portland, OR',
+          style: 'IPA - West Coast',
+          abv: 7.3,
+          ibu: 62,
+          price: 7,
+          pintsLeft: 124
+        }
+      ]
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={BeerList} />
+          <Route exact path='/addbeer' component={AdminControl} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    )
+  };
 }
 
 export default App;
--- src/components/BeerList/BeerList.jsx
+++ src/components/BeerList/BeerList.jsx
@@ -7,69 +7,6 @@ import tricerahops from '../../assets/labels/tricerahops.jpg';
 import saisondelis from '../../assets/labels/saisondelis.jpeg';
 import denimvestpionage from '../../assets/labels/denimvestpionage.jpg';
 
-var masterBeerList = [
-  {
-    label: oldrasputin,
-    name: 'Old Rasputin (Nitro)',
-    brewery: 'North Coast Brewing Company, Fort Bragg, CA',
-    style: 'Stout - Russian Imperial',
-    abv: 9,
-    ibu: 75,
-    price: 8,
-    pintsLeft: 124
-  },
-  {
-    label: papyrusiris,
-    name: 'Papyrus Iris',
-    brewery: 'Ruse Brewing, Portland, OR',
-    style: 'IPA - New England',
-    abv: 6.5,
-    ibu: 37,
-    price: 7,
-    pintsLeft: 124
-  },
-  {
-    label: guavasocksandsandals,
-    name: 'Guava Socks & Sandals',
-    brewery: 'Stickmen Brewing Company, Lake Oswego, OR',
-    style: 'IPA - New England',
-    abv: 6.6,
-    ibu: 37,
-    price: 7,
-    pintsLeft: 124
-  },
-  {
-    label: tricerahops,
-    name: 'Tricerahops',
-    brewery: 'Ninkasi Brewing Company, Eugene, OR',
-    style: 'IPA - Double West Coast',
-    abv: 8,
-    ibu: 84,
-    price: 7,
-    pintsLeft: 124
-  },
-  {
-    label: saisondelis,
-    name: 'Saison de Lis',
-    brewery: 'Perennial, St Louis, MO',
-    style: 'Farmhouse Ale - Saison',
-    abv: 5,
-    ibu: 20,
-    price: 8,
-    pintsLeft: 124
-  },
-  {
-    label: denimvestpionage,
-    name: 'Denim Vest-pionage',
-    brewery: 'Baerlic, Portland, OR',
-    style: 'IPA - West Coast',
-    abv: 7.3,
-    ibu: 62,
-    price: 7,
-    pintsLeft: 124
-  }
-];
-
 function BeerList(){
   return (
     <div>
