fca53ea1ec620fba1cd1d1b139101ff802274473
Marc Davies <marcdaviesriot@gmail.com>
2019-07-16 23:51:21 -0700

    add state to Admin page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 class Admin extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      adminPanelVisibleOnPage: false
+    };
+  }
+
   render(){
     return (
       <div>
